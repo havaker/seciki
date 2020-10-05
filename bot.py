a44af0f1d465981082a5cdad6d8cc7f7fa89671f
Michał Sala <0havaker@gmail.com>
2020-10-05 23:20:26 +0200

    Keep track of downloaded songs

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import subprocess
+import os
 
 from dataclasses import dataclass
+from xdg import XDG_DATA_HOME
 
 from scdl import client
 from scdl import scdl
@@ -8,7 +10,8 @@ from scdl import scdl
 @dataclass
 class Config:
     username: str
-    path: str = None
+    download_path: str = None
+    index_path: str = os.path.join(XDG_DATA_HOME, "seciki/index.txt")
 
 class FavsLister:
     def __init__(self, config):
@@ -22,7 +25,7 @@ class FavsLister:
 
 class Downloader:
     def __init__(self, config):
-        self.path = config.path
+        self.path = config.download_path
 
     def download(self, link):
         command = ["scdl", "--onlymp3", "-l", link]
@@ -31,9 +34,35 @@ class Downloader:
         else:
             subprocess.call(command)
 
-config = Config("djmcmostowiak", "hej/")
+class Index:
+    def __init__(self, config):
+        self.path = config.index_path
+        dirname = os.path.dirname(self.path)
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
 
+    def load(self):
+        try:
+            with open(self.path) as f:
+                return [line.rstrip() for line in f]
+        except:
+            return []
+
+    def save(self, url):
+        with open(self.path, 'a') as f:
+            f.write(url + '\n')
+
+config = Config("phem4evr", "hej/")
+
+index = Index(config)
 lister = FavsLister(config)
 downloader = Downloader(config)
 
-downloader.download(lister.list_urls()[0])
+fav_urls = lister.list_urls()
+saved_urls = index.load()
+
+unsaved_urls = set(fav_urls) - set(saved_urls)
+
+for url in unsaved_urls:
+    downloader.download(url)
+    index.save(url)
